a3e4f5044c5a20ed1c526d87b8258f5f89f27585
Bibi <brieuc@bdaniel.fr>
2015-03-14 09:56:57 +0100

    Add mitsuba.sh a bash script making screenshots at a regular time and exporting CPU and Memory use

---
+++ mitsuba.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+mitsuba "${1}" &
+echo "${1} - Début du traitement" > log.txt
+((i = 0))
+while pgrep mitsuba>/dev/null
+	do
+		date >> log.txt
+		if  [ $i -gt 1 ]
+			then killall -HUP mitsuba
+		fi
+		sleep 5
+		if [ -f ${1//.*}.exr ];
+			then mv ${1//.*}.exr ${1//.*}_${i}.exr
+		fi
+		top -n 1 -b | grep mitsuba >> log.txt
+		sleep 55
+		((i++))
+done
